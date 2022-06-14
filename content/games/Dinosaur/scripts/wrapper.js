3c98608af5365769a2aeae6adfdcca18126332b4
MCmoderSD <MCmoderSD@web.de>
2022-06-14 13:19:21 +0200

    init

---
+++ content/games/Dinosaur/scripts/wrapper.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+    new Runner('.interstitial-wrapper');
+  } else {
+    document.getElementById("main-frame-notchrome").style.display="";
+  }
---
+++ pihole/index.php
@@ -0,0 +1,14 @@
+<?php?>
+<html>
+        <head>
+            <title>Error - 404</title>
+            <link rel="stylesheet" href="/content/style/error_style.css">
+        </head>
+    
+        <body>
+            <div id="error">Error - 404</div>
+            <div id="gif"><img src="/content/memes/404-meme.gif"></div>
+            <div id="description">this site doesn't exist!</div>
+            <div id="back"><a href="/index.html">back</a></div>
+	</body>
+</htlm>
