223d4485e0ead8a0c168a3b56a764de65ff507f9
MCmoderSD <MCmoderSD@web.de>
2022-06-15 14:35:23 +0200

    php

--- content/twitch/Twitch-Clips.php
+++ content/twitch/Twitch-Clips.php
@@ -10,7 +10,7 @@
         <!--<a href="/content/twitch/clips/dacs-clips.html">dacs</a>-->   <a href="/content/twitch/clips/r4kunnn-clips.php">r4kunnn</a>
    </div> 
    <br/>
-      <div id="back">
+      <div class="back">
           <a href="/index.php">back</a>
       </div> 
 	</body>
