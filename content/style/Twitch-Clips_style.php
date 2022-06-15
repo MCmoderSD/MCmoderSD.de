1710ecae8a4d4e14bec4e06f22534ef5e89ac16e
MCmoderSD <MCmoderSD@web.de>
2022-06-15 17:06:27 +0200

    php

--- content/style/Twitch-Clips_style.php
+++ content/style/Twitch-Clips_style.php
@@ -3,14 +3,20 @@ header('content-type: text/style; charset=utf-8');
 ?>
 
 body {
-	background-color: #36393f;
-    padding: 80px;
-    text-align: center;
-  }
+background-color: #36393f;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
+align-items: center;
+}
 
 .center {
-  margin-left: auto;
-  margin-right: auto;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
+align-items: center;
 }
 
 .links a {
@@ -21,16 +27,18 @@ body {
   padding: 20px;
 }
 
-.back {
-  color: rgb(88, 101, 242);
-  font-size: 32px;
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
+.back a {
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
 }
 
-.back a {
-  color: rgb(88, 101, 242);
-  font-size: 32px;
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
+.back {
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
 }
--- content/style/clips_style.php
+++ content/style/clips_style.php
@@ -3,26 +3,32 @@ header('content-type: text/style; charset=utf-8');
 ?>
 
 body {
-	background-color: #36393f;
-    padding: 20px;
-    text-align: center;
+background-color: #36393f;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
 }
 
 .center {
-  margin-left: auto;
-  margin-right: auto;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
 }
 
 .back a {
-  color: rgb(88, 101, 242);
-  font-size: 32px;
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
 }
 
 .back {
-    color: rgb(88, 101, 242);
-    font-size: 32px;
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
 }
