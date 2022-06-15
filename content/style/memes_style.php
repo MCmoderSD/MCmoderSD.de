4368cc6614c16b6c7e858b347e0f5da411ccf055
MCmoderSD <MCmoderSD@web.de>
2022-06-15 17:09:17 +0200

    php

--- content/style/memes_style.php
+++ content/style/memes_style.php
@@ -3,30 +3,32 @@ header('content-type: text/style; charset=utf-8');
 ?>
 
 body {
-	background-color: #36393f;
-    padding: 20px;
-    align-items: center;
-    text-align: center;
+background-color: #36393f;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
 }
 
 .center {
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: auto;
-  margin-bottom: auto;
-  align-items: center;
-}
-
-.back {
-    color: rgb(88, 101, 242);
-    font-size: 32px;
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
+margin-left: auto;
+margin-right: auto;
+margin-top: auto;
+margin-bottom: auto;
 }
 
 .back a {
-    color: rgb(88, 101, 242);
-    font-size: 32px;
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
+}
+
+.back {
+color: rgb(88, 101, 242);
+font-size: 32px;
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+padding: 20px;
 }
