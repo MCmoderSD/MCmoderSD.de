1edd616392b6c7110e5dff9ce8eddf69c5b4c5fb
MCmoderSD <MCmoderSD@web.de>
2022-06-16 04:30:14 +0200

    discord

---
+++ content/discordbot/discordbot.php
@@ -0,0 +1,18 @@
+<?php ?>
+<html lang="de">
+
+<head>
+    <title>Discord Bot</title>
+    <link rel="stylesheet" href="/content/style/discordbot_style.php">
+</head>
+
+<body>
+
+<div class="links"><a href="https://discord.com/api/oauth2/authorize?client_id=954136734482989096&permissions=3148800&scope=bot">Hochverdiener</a> <a href="https://discord.com/oauth2/authorize?client_id=986802937177182228&permissions=3155968&scope=bot">Hochverdiener [LITE]</a></div>
+<br/>
+<div class="back">
+    <a href="/index.php">back</a>
+</div>
+
+</body>
+</html>
