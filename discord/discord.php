25da67b78d7435ca15b3492f3e972ad79edaff1a
MCmoderSD <MCmoderSD@web.de>
2022-07-03 17:07:59 +0200

    discord

--- discord/discord.php
+++ discord/discord.php
@@ -10,6 +10,7 @@
 
 <div class="links"><a href="https://discord.com/api/oauth2/authorize?client_id=954136734482989096&permissions=3148800&scope=bot">Hochverdiener</a> <a href="https://discord.com/oauth2/authorize?client_id=986802937177182228&permissions=3155968&scope=bot">Hochverdiener [LITE]</a></div>
 <br/>
+<div class="links"><a href="https://discord.gg/FZRjzeYJsJ">Join Server</a></div>
 <div class="back">
     <a href="/index.php">back</a>
 </div>
