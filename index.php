d91e76a6709df9ffcfa686f43dfc6b995250d014
MCmoderSD <MCmoderSD@web.de>
2022-09-09 18:49:30 +0200

    index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <br><br/>
 
 <div id="links"><a href="content/games/games.php">Games</a> <a href="/content/cookie/index.php">Cookies</a> <a href="/content/cracks/cracks.php">Cracks</a> <a href="/content/discord/discord.php">DiscordBot</a> <a href="/content/memes/memes.php">Memes</a> <a href="content/music/music.php">Musik</a> <a href="/content/twitch/Twitch-Clips.php">Twitch Clips</a></div>
-<div id="proxy"><a href="/admin/admincontrol.php">Admin-Center</a> | Proxy: IP 178.254.18.170 | Port 3128</div>
+<!--<<div id="proxy"><a href="/admin/admincontrol.php">Admin-Center</a> | Proxy: IP 178.254.18.170 | Port 3128</div>-->
 
 <!--<a href="/content/login/login.html">Log in</a><a href="signup">Sign up</a>-->
 
