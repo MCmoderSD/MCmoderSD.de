864cfe602bb568fab63e39443faf4d6f4822c91d
MCmoderSD <mcmodersd@web.de>
2022-09-29 17:40:54 +0200

    hochverdienerV2

--- phpscripts/uploader.php
+++ phpscripts/uploader.php
@@ -15,7 +15,6 @@ if (isset($_POST['submit'])) {
                 $fileNameNew = uniqid('', true).".".$fileActualExt;
                 $fileDestination = '/uploads/'.$fileNameNew;
                 move_uploaded_file($fileTmpName, $fileDestination);
-                header("Location: index.php?uploadsuccess");
             } else {
                 echo "Your file is too big!";
             }
