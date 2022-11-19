6f641033d0cb0d801fa66fb121e50cbaad314fd1
Seraphin Berger <MCmoderSD@web.de>
2022-11-19 04:12:29 +0100

    Upadtes

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>MCmoderSD.live</title>
     <link rel="stylesheet" type="text/css" href="/content/style/index_style.php"/>
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2858567818610194"
+            crossorigin="anonymous"></script>
 </head>
 
 <body>
