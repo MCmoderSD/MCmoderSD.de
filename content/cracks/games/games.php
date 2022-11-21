126cc6586f7c3bcfb1287eed270619ab82a8866f
Seraphin Berger <MCmoderSD@web.de>
2022-11-21 21:29:14 +0100

    Upadtes

--- content/cracks/games/games.php
+++ content/cracks/games/games.php
@@ -8,66 +8,48 @@
 </head>
 
 <body>
-
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=636bcdb495b29b17">A Plague
-        Tale Innocence</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=d0eafe0ed7992dfd">A Plague Tale Innocence</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=631666e537e6601f">Animal
-        Revolt Battle Simulator</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=631666e537e6601f">Animal Revolt Battle Simulator</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=e7d9c7dac2ef53e6">Death
-        Stranding Directors Cut</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=e7d9c7dac2ef53e6">Death Stranding Directors Cut</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=3419e1bdeca63c2c">Getting
-        Over It</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=3419e1bdeca63c2c">Getting Over It</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=2627a699a93e4cfa">Hardspace
-        Shipbreaker</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=2627a699a93e4cfa">Hardspace Shipbreaker</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=7a46de3ee162d59d">Horizon
-        Zero Dawn</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=9bd5c197264697a8">Horizon Zero Dawn</a></div>
 <br>
-<!--<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=">Jurassic Park The Game</a></div>
-<br>-->
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=ad6f941bab9d6ce6">Kanojo
-        VR</a></div>
+<!--<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=">Jurassic Park The Game</a></div>-->
+<!--<br>-->
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=ad6f941bab9d6ce6">Kanojo VR</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=0cb8238a0d6b68db">LEGO
-        Builders Journey</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=0cb8238a0d6b68db">LEGO Builders Journey</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=42a9b1458ab9787c">Leisure
-        Suit Larry Wet Dreams Dry Twice</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=42a9b1458ab9787c">Leisure Suit Larry Wet Dreams Dry Twice</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=bbbcbe2b5031cd2d">Minecraft
-        Story Mode</a>
-</div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=bbbcbe2b5031cd2d">Minecraft Story Mode</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=07ea65070df65617">Minecraft
-        Story Mode Season
-        Two</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=07ea65070df65617">Minecraft Story Mode Season Two</a></div>
 <br>
-<div class="links"><a
-            href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=a9cae58bf8a240e2">Narcosis</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=7879a93be3398411">Narcosis</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=97f026fcefaaaedc">Sims 4</a>
-</div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=9c58a38fe234f0d4">PC Building Simulator</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=86fedd2fa74449dc">Stray</a>
-</div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=12352dae25c1aefc">PC Building Simulator 2</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=5f9f820d2613ef37">Superliminal</a>
-</div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=97f026fcefaaaedc">Sims 4</a></div>
 <br>
-<div class="links"><a
-            href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=8e74e78957b27ee9">Teardown</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=86fedd2fa74449dc">Stray</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=20eed245943bc61f">The Dark
-        Pictures Anthology Man of Medan</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=bef8e735ded92229">Superliminal</a></div>
 <br>
-<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=9d6e0a6114dfa86a">The Stanley
-        Parable Ultimate Deluxe</a></div>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=8e74e78957b27ee9">Teardown</a></div>
 <br>
-
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=a466b4f409daedeb">The Dark Pictures Anthology Man of Medan</a></div>
+<br>
+<div class="links"><a href="https://z9iswj91nn31eabm.myfritz.net:47201/nas/filelink.lua?id=9d6e0a6114dfa86a">The Stanley Parable Ultimate Deluxe</a></div>
+<br>
 <div class="back">
     <a href="/content/cracks/cracks.php">back</a>
 </div>
