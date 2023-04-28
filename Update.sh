46804f09a28e10b9abf4ac3eb5aff6371092a52a
Seraphin Berger <82591988+MCmoderSD@users.noreply.github.com>
2023-04-28 10:22:40 +0200

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,14 +2,14 @@
 
 if [ "$1" = "-auto" ]; then
   while true; do
-    sudo git -C /var/www/mcmodersd.live/ pull
+    sudo git pull https://www.github.com/mcmodersd/mcmodersd.live.git
     sudo rm -r /etc/apache2/sites-enabled/mcmodersd.live.conf
     ln -sf /var/www/mcmodersd.live/mcmodersd.live.conf /etc/apache2/sites-enabled/mcmodersd.live.conf
     sudo systemctl reload apache2
     sleep 30
   done
 else
-  sudo git -C /var/www/mcmodersd.live/ pull
+  sudo git pull https://www.github.com/mcmodersd/mcmodersd.live.git
   sudo rm -r /etc/apache2/sites-enabled/mcmodersd.live.conf
   ln -sf /var/www/mcmodersd.live/mcmodersd.live.conf /etc/apache2/sites-enabled/mcmodersd.live.conf
   sudo systemctl reload apache2
