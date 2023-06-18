571ee163483682d98d60244a821a51ba4c543e14
Seraphin Berger <MCmoderSD@web.de>
2023-06-18 13:05:03 +0200

    fix

--- Update.sh
+++ Update.sh
@@ -7,15 +7,15 @@ if [ "$1" = "-auto" ]; then
   fi
 
   while true; do
-    sudo git pull https://www.github.com/mcmodersd/mcmodersd.de.git
-    sudo rm -r /etc/apache2/sites-available/mcmodersd.de.conf
-    ln -sf /var/www/mcmodersd.de/mcmodersd.de.conf /etc/apache2/sites-available/mcmodersd.de.conf
+    sudo git pull https://www.github.com/MCmoderSD/MCmoderSD.de.git
+    sudo rm -r /etc/apache2/sites-available/MCmoderSD.de.conf
+    ln -sf /var/www/MCmoderSD.de/MCmoderSD.de.conf /etc/apache2/sites-available/MCmoderSD.de.conf
     sudo systemctl reload apache2
     sleep "$2"
   done
 else
-  sudo git pull https://www.github.com/mcmodersd/mcmodersd.de.git
-  sudo rm -r /etc/apache2/sites-available/mcmodersd.de.conf
-  ln -sf /var/www/mcmodersd.de/mcmodersd.de.conf /etc/apache2/sites-available/mcmodersd.de.conf
+  sudo git pull https://www.github.com/MCmoderSD/MCmoderSD.de.git
+  sudo rm -r /etc/apache2/sites-available/MCmoderSD.de.conf
+  ln -sf /var/www/MCmoderSD.de/MCmoderSD.de.conf /etc/apache2/sites-available/MCmoderSD.de.conf
   sudo systemctl reload apache2
 fi
