dba8f28f407a65c22b1b22fe2acbb4de2c16a3db
MCmoderSD <mail@mcmodersd.de>
2026-07-24 19:27:15 +0200

    Added PrivacyPolicyPageComponent

---
+++ src/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy-page',
+  templateUrl: './privacy-policy-page.component.html',
+  styleUrl: './privacy-policy-page.component.scss',
+  standalone: false,
+})
+export class PrivacyPolicyPageComponent { }
